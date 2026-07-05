75957ededa8b7d57322deab8a28b4c7fa90f9853
Debiprasad <debiprasad_dash1@outlook.com>
2026-07-05 15:39:31 +0200

    session_37

---
+++ Window_function/session_37_Q1.sql
@@ -0,0 +1,3 @@
+SELECT * FROM (SELECT battingteam,batter,SUM(batsman_run) as 'totalrun',
+DENSE_RANK() OVER(partition by battingteam order by SUM(batsman_run) DESC) AS 'rank'
+FROM IPl GROUP BY battingteam,batter) t WHERE t.rank <=5 ORDER BY t.battingteam,t.rank
